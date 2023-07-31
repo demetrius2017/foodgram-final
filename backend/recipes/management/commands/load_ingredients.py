607dbc878296517ab47b31af84bbcb24df286a2d
Dmitry N <demetrius.nazarov@yandex.ru>
2023-07-31 21:09:06 +0200

    PEP8

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -7,18 +7,12 @@ from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-    help = 'Load data from csv files'
+    help = "Load data from csv files"
 
     def handle(self, *args, **kwargs):
         data_path = settings.BASE_DIR
-        with open(
-            f'{data_path}/data/ingredients.csv',
-            'r',
-            encoding='utf-8'
-        ) as file:
+        with open(f"{data_path}/data/ingredients.csv", "r", encoding="utf-8") as file:
             reader = csv.DictReader(file)
-            Ingredient.objects.bulk_create(
-                Ingredient(**data) for data in reader
-            )
+            Ingredient.objects.bulk_create(Ingredient(**data) for data in reader)
 
-        self.stdout.write(self.style.SUCCESS('Successfully load data'))
+        self.stdout.write(self.style.SUCCESS("Successfully load data"))
--- backend/recipes/management/commands/load_tags.py
+++ backend/recipes/management/commands/load_tags.py
@@ -1,17 +1,16 @@
 from django.core.management import BaseCommand
-
 from recipes.models import Tag
 
 
 class Command(BaseCommand):
-    help = 'Create default tags'
+    help = "Create default tags"
 
     def handle(self, *args, **kwargs):
         data = [
-            {'name': 'Завтрак', 'color': '#E26C2D', 'slug': 'breakfast'},
-            {'name': 'Обед', 'color': '#49B64E', 'slug': 'dinner'},
-            {'name': 'Ужин', 'color': '#8775D2', 'slug': 'supper'},
+            {"name": "Завтрак", "color": "#E26C2D", "slug": "breakfast"},
+            {"name": "Обед", "color": "#49B64E", "slug": "dinner"},
+            {"name": "Ужин", "color": "#8775D2", "slug": "supper"},
         ]
         Tag.objects.bulk_create(Tag(**tag) for tag in data)
 
-        self.stdout.write(self.style.SUCCESS('Successfully create tags'))
+        self.stdout.write(self.style.SUCCESS("Successfully create tags"))
