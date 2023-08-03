599aa9b0092925cb18ebd2fb4bdc71dd646bc787
Dmitry N <demetrius.nazarov@yandex.ru>
2023-08-03 18:46:45 +0200

    back test

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     email = models.EmailField(
         "Email",
@@ -10,10 +11,10 @@ class User(AbstractUser):
     first_name = models.CharField("Имя", max_length=150)
     last_name = models.CharField("Фамилия", max_length=150)
     shopping_cart = models.OneToOneField(
-        'recipes.ShoppingCart', on_delete=models.SET_NULL, null=True
+        "recipes.ShoppingCart", on_delete=models.SET_NULL, null=True
     )
     favorite_recipes = models.ManyToManyField(
-        'recipes.Recipe', related_name="favorited_by_users"
+        "recipes.Recipe", related_name="favorited_by_users"
     )
 
     USERNAME_FIELD = "email"
