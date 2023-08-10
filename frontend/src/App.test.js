a83703310d0a7fd6ef2a5c6e5d69e9372ebfc86e
Dmitry N <demetrius.nazarov@yandex.ru>
2023-08-10 23:14:48 +0200

    Main

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,13 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders login title', () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-  const titleElement = screen.getByText(/Войти на сайт/i);
-  expect(titleElement).toBeInTheDocument();
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
